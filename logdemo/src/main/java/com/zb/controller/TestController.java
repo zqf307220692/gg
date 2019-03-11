8c54977ae739a41c0a0c6cd1f8cae874c9dffd5a
zqf <307220692@qq.com>
2019-03-11 11:49:52 +0800

    update

--- logdemo/src/main/java/com/zb/controller/TestController.java
+++ logdemo/src/main/java/com/zb/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
+    //111
     @Autowired
     public KafkaTemplate<String,String> kafkaTemplate;
 
